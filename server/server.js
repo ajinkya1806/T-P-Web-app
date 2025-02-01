f83b3c7b943ada0d83c1a193d7dfa83dd86016dc
DanChandargi7 <danishchandargi@gmail.com>
2025-02-01 15:41:14 +0530

    db connection done

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
+require("dotenv").config(); // Load environment variables
+const db = require("./config/db");
 const port = 5000; // Port number for your backend
 
 // Middleware to parse JSON requests
 app.use(express.json());
+app.use(cors());
 
 // Basic Route
 app.get('/', (req, res) => {
